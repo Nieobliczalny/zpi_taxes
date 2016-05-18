e74ea793a9d0efaea1d782e040ac878ce89d0b9b
myszaa <michal2b@o2.pl>
2016-05-18 15:29:30 +0200

    pusty projekt

---
+++ st/src/main/java/Stany/st/App.java
@@ -0,0 +1,13 @@
+package Stany.st;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
