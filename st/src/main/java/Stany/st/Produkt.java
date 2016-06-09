22569a9c5338161411d60ca3695db7d29957055b
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-06-09 15:25:31 +0200

    Dodanie kategori i nowych produktow

--- st/src/main/java/Stany/st/Produkt.java
+++ st/src/main/java/Stany/st/Produkt.java
@@ -3,17 +3,27 @@ package Stany.st;
 public class Produkt {
 	@Override
 	public String toString() {
-		return "nazwa produktu: " + nazwa + " cena = " + cena;
+		return "nazwa produktu: " + nazwa + " cena = " + cena + " kategoria = " + kategoria;
 	}
 
 	private String nazwa = "";
 	private double cena;
+	private String kategoria;
 
 	public Produkt(String linia) {
 		String s = linia;
 		String[] tokens = s.split(",");
 		nazwa = tokens[0];
-		cena = Double.valueOf(tokens[1]);			 	
+		cena = Double.valueOf(tokens[1]);
+		kategoria = tokens[2];
+	}
+
+	public String getKategoria() {
+		return kategoria;
+	}
+
+	public void setKategoria(String kategoria) {
+		this.kategoria = kategoria;
 	}
 
 	public String getNazwa() {
